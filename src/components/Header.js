9da225be124e660cfc1bfed2c4f9755d57af9b74
jean young park <jean.young.park.96@gmail.com>
2023-02-15 20:05:20 +0900

    동작하지 않는 기능 삭제

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 export default function Header() {
   const location = useLocation();
-  const [userLocation, setUserLocation] = useState(location.pathname);
 
   return (
     <header>
-        <Link to="/signup" className={userLocation === 'signup' ? 'on' : ''} onClick={() => setUserLocation('signup')}>signup</Link>
-        <Link to="/signin"  className={userLocation === 'signin' ? 'on' : ''} onClick={() => setUserLocation('signin')}>signin</Link>
+        <Link to="/signup">signup</Link>
+        <Link to="/signin">signin</Link>
     </header>
   )
 }
